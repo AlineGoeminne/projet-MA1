9d4bd1a2e277685a3e777a158cf78cfa270c3e2a
Aline <goeminne.aline@gmail.com>
2016-04-27 14:42:17 +0200

    classe représentant un jeu d'atteignabilité

---
+++ implémentation/AtteignabilityGame.py
@@ -0,0 +1,57 @@
+
+class Vertex(object):
+    """
+        Classe representant un sommet d'un graphe, on considere que ces sommets sont entierement determines par leur
+        "nom" qui est en fait un entier.
+    """
+    def __init__(self, i, player):
+        self.name = i
+        self.player = player
+
+    def __eq__(self, other):
+
+        if id(self) == id(other):
+            return True
+        else:
+            return self.name == other.name
+
+
+class Graph(object):
+
+    """
+
+    :param vertex : liste des sommets du jeu classe par leur ID
+    :type vertex :  list
+
+    :param mat : matrice d adjacence du jeu
+    :type mat : tableau |vertex|*|vertex|
+
+    :param pred: liste de predecesseurs. Pour chaque noeud v, on associe une liste de tuples (p,w) ou p est un de ces
+    predecesseurs et w est le poid de l'arc (p,v)
+    :type pred: liste de listes de tuples
+
+    :param succ: liste de successeurs. Pour chauqe noeud v, on associe une liste de tuples (s,w) ou s est un ces succ
+    et w est le poids de l'arc (v,s)
+    :type succ: liste de listes de tuples
+
+    """
+
+    def __init__(self, vertex, mat , pred, succ):
+
+        self.vertex = vertex
+
+        self.mat = mat
+        self.pred = pred
+        self. succ = succ
+
+
+class ReachabilityGame(object):
+
+    def __init__(self, player, graph, goal, partition):
+
+        self.player = player
+        self.graph = graph
+        self.goal = goal
+        self.part = partition
+
+
