9363ad86fb947cbec2f1d7d8b029a0e884322f0d
Aline Goeminne <goeminne.aline@gmail.com>
2017-05-15 16:04:07 +0200

    test du plus court chemin

--- implémentation/Test/ShortestPathTest.py
+++ implémentation/Test/ShortestPathTest.py
@@ -32,6 +32,20 @@ class TestShortestPath(unittest.TestCase):
 
         self.assertEqual(graph.floyd_warshall(),[[0,-1,-2,0],[4,0,2,4],[5,1,0,2],[3,-1,1,0]])
 
+    def test_shortest_path_with_tuple(self):
+
+        mat = [[(np.inf,np.inf),(np.inf,0),(-2,4),(np.inf,1)], [(4,np.inf),(np.inf,np.inf),(3,np.inf),(np.inf,1)],
+               [(np.inf,1),(np.inf,np.inf),(np.inf,np.inf),(2,2)],[(np.inf,-5),(-1,np.inf),(np.inf,np.inf),(np.inf,np.inf)]]
+
+        graph = Graph(None, mat, None, None, None)
+
+        #on verifie que cela fonctionne si on calcule pour une seule composante du tuple
+        self.assertEqual(graph.floyd_warshall(True,0),[[0,-1,-2,0],[4,0,2,4],[5,1,0,2],[3,-1,1,0]])
+        with self.assertRaises(NegativeCircuitError):
+            graph.floyd_warshall(True,1)
+        #on verifie que le calcul des projections fonctionne bien
+        self.assertEqual(Graph.mat_proj_componant(mat,0),[[np.inf,np.inf,-2,np.inf], [4,np.inf,3,np.inf], [np.inf,np.inf,np.inf,2],[np.inf,-1,np.inf,np.inf]])
+        self.assertEqual(Graph.mat_proj_componant(mat,1),[[np.inf, 0, 4, 1], [ np.inf, np.inf,np.inf,1], [1, np.inf,np.inf,2],[-5,np.inf,np.inf,np.inf]])
 
 if __name__ == '__main__':
 
